c566d0f39990545db607d0a332dad945415a0ffa
André Ximenes <andre1ximenes92@gmail.com>
2024-07-19 16:19:28 -0300

    Array

---
+++ 02 - Array/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string[] names = {"Andrew", "Marcos", "Vinicius", "Paulo"};
+
+        //Printing all elements of the Array
+        
+        /*for(int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+        }*/
+        
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
+
