feee9c541c3505222a10105812edee37880525d0
André Ximenes <andre1ximenes92@gmail.com>
2024-07-19 13:40:32 -0300

    01 - calcula idade

---
+++ 01 - Calcula idade/Program.cs
@@ -0,0 +1,32 @@
+﻿//Calculation your age
+
+int currentYear = 2024;
+string name;
+int yearBirth;
+int age;
+int legalAge = 18;
+
+Console.Write("Name: ");
+name = Console.ReadLine();
+Console.Write("Year of birth: ");
+yearBirth = int.Parse(Console.ReadLine());
+
+//creates a line
+Console.WriteLine(string.Concat(Enumerable.Repeat("_", 50)));
+
+//receives the calculation between the current year and the date of birth
+age = currentYear - yearBirth;
+
+Console.WriteLine($"{name}, you are {age} years old");
+
+//verifies that you are of legal ange
+if(age >= legalAge)
+{
+    Console.WriteLine("You are of legal age!");
+}
+else
+{
+    Console.WriteLine("Your are not of legal age!");
+};
+
+
