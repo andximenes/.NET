259c35b585bf8d78de5bb2d747430cbda040af58
André Ximenes <andre1ximenes92@gmail.com>
2024-07-22 16:07:05 -0300

    03 - Comparação

---
+++ 03 - Comparação/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string[] names = {"Andrew", "Bruno", "Cláudio"};
+
+if(string.Equals(names[0], "andrew", StringComparison.OrdinalIgnoreCase))
+{
+    Console.WriteLine("Same names");
+}
+else
+{
+    Console.WriteLine("Different names");
+}
+
+
+bool equals = string.Equals(names[0], "andrew", StringComparison.OrdinalIgnoreCase);
+Console.WriteLine(equals);
